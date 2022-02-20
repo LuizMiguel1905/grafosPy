b79df926fc35d24fd7f0f52e5d5288499726553d
Tiago Ary <tiago.ary10@gmail.com>
2022-02-20 11:37:43 -0300

    refatorando a classe Grafo

--- models/grafo.py
+++ models/grafo.py
@@ -4,35 +4,39 @@ from collections import defaultdict
 class Grafo(object):
     """ Implementação básica de um grafo. """
 
-    def __init__(self, arestas, direcionado=False):
+    def __init__(self, vertices, direcionado=False):
         """Inicializa as estruturas base do grafo."""
-        self.adj = defaultdict(set)
+        self.adj = defaultdict()
         self.direcionado = direcionado
-        self.adiciona_arestas(arestas)
+        self.adiciona_vertices(vertices)
 
+    def add_arestas_by_adj_matrix(self, matrix):
+        """ Adiciona as conexãões aos vértices através da """
+        for v in self.get_vertices():
+            self.adj[v] = self.has_connections_by_line(matrix[v])
 
     def get_vertices(self):
         """ Retorna a lista de vértices do grafo. """
         return list(self.adj.keys())
 
+    def has_connections_by_line(self, line):
+        """ Retorna uma lista dos vértices representados em uma linha da matriz"""
+        connect_list = list()
+        for n in range(len(line)):
+            if line[n] == 1:
+                connect_list.append(self.get_vertices()[n])
+        return connect_list
 
     def get_arestas(self):
         """ Retorna a lista de arestas do grafo. """
         return [(k, v) for k in self.adj.keys() for v in self.adj[k]]
 
 
-    def adiciona_arestas(self, arestas):
-        """ Adiciona arestas ao grafo. """
-        for u, v in arestas:
-            self.adiciona_arco(u, v)
+    def adiciona_vertices(self, vertices):
+        """ Adiciona vértices ao grafo. """
+        for v in vertices:
+            self.adj[v] = []
 
-
-    def adiciona_arco(self, u, v):
-        """ Adiciona uma ligação (arco) entre os nodos 'u' e 'v'. """
-        self.adj[u].add(v)
-        # Se o grafo é não-direcionado, precisamos adicionar arcos nos dois sentidos.
-        if not self.direcionado:
-            self.adj[v].add(u)
 
 
     def existe_aresta(self, u, v):
