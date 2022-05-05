a9ef1ebebc06266a4cae1d9172b78875023f8042
LuizM <luizmgc1930@gmail.com>
2022-05-05 01:25:31 -0300

    update

--- Atividade06.py
+++ Atividade06.py
@@ -7,10 +7,26 @@ from utils.ConverterUtil import ConverterUtil
 from utils.ValidadorUtil import ValidadorUtil
 from utils.PathUtil import PathUtil
 
-grafo1 = pd.read_excel("docs/grafos/Grafos.xlsx", sheet_name='Grafo 8')
-grafo2 = pd.read_excel("docs/grafos/Grafos.xlsx", sheet_name='Grafo 9')
+from utils.Dijsktra import Dijsktra
 
+grafo1 = pd.read_excel("docs/grafos/grafoII.xlsx", sheet_name='grafo1')
+grafo2 = pd.read_excel("docs/grafos/grafoII.xlsx", sheet_name='grafo2')
+#grafo3 = pd.read_excel("docs/grafos/grafoII.xlsx", sheet_name='grafo3')
 converterUtil = ConverterUtil()
 validadorUtil = ValidadorUtil()
 pathUtil = PathUtil()
+
+disk = Dijsktra()
+
+#graph1 = Graph(ponderado=True,  direcionado=True)
+graph2 = Graph(ponderado=True,  direcionado=True)
+#graph3 = Graph(ponderado=True,  direcionado=False)
+#graph1.addEdgeByAdjMatrix(converterUtil.convertMatrixToDict(grafo1))
+graph2.addEdgeByAdjMatrix(converterUtil.convertMatrixToDict(grafo2))
+#graph3.addEdgeByAdjMatrix(converterUtil.convertMatrixToDict(grafo3))
+
+#disk.dijkstra(graph1, "A")
+disk.dijkstra(graph2, "A")
+
+
 
--- utils/Dijsktra.py
+++ utils/Dijsktra.py
@@ -1,78 +1,44 @@
-
-#from models.grafo import Graph
+from models.grafo import Graph
 
 from queue import PriorityQueue
 
 class Dijsktra:
-    def __init__(self, quant_de_nos):
-        #grafo = Graph()
-        
+   
+    def __init__(self) -> None:
+        pass
 
-        #Numero de vertices
+    def dijkstra(self, grafo, start_vertex):
+        self.visited  = []
 
-        self.v = quant_de_nos
-        #self.v = range(grafo.V)
-        self.edge = [ [-1 for i in range(quant_de_nos)] for j in range(quant_de_nos) ] 
-        
-        #Vertices visitados
-        self.visited = [] 
+        D = {v:float('inf') for v in range(len(grafo.V))}
 
-    #Adicionar as arestas com seus respectivos pesos
-    def add_edge(self, u, v, weight):
-        self.edge[u][v] = weight
-        self.edge[v][u] = weight
-    
-    def dijkstra(self, start_vertex):
+        D[grafo.V.index(start_vertex)] = 0
 
-        
-        #Criar uma lista D de tamanho V
-        D = {v:float('inf') for v in range(self.v)}
-
-        D[start_vertex] = 0
-
-        #Lista de prioridade
         listaP = PriorityQueue()
         listaP.put((0, start_vertex))
 
-        #Loop só acabara quando todos os nós estiverem em D
+
         while not listaP.empty():
             (dist, no_atual) = listaP.get()
             self.visited.append(no_atual)
+            
+            
+            for adjacente in grafo.graph[no_atual]:
+                for edges, peso in grafo.edges:
+                    if edges == (no_atual, adjacente):
+                        distance = peso
+                        break
 
-            #Loop para percorrer todos os nós
-            for adjacente in range(self.v):
-                print(self.edge[no_atual])
-                if self.edge[no_atual][adjacente] != -1:
-                    distance = self.edge[no_atual][adjacente]
-                    #Caso o nó adjacente não for visitado
-                    if adjacente not in self.visited:
 
+                if adjacente not in self.visited:
+
+                    custo_anterior = D[grafo.V.index(adjacente)]
+                    novo_custo = D[grafo.V.index(no_atual)] + distance
+                    
+                    if novo_custo < custo_anterior:
                         
-                        custo_anterior = D[adjacente]
-                        novo_custo = D[no_atual] + distance
+                        listaP.put((novo_custo, adjacente))
+                        D[grafo.V.index(adjacente)] = novo_custo
 
-
-                        #Caso o ó antigo seja maior que o novo
-                        #O custo nnovo sera adicionado a
-                        if novo_custo < custo_anterior:
-
-                            listaP.put((novo_custo, adjacente))
-                            D[adjacente] = novo_custo
-        return D
-
-g = Dijsktra(6)
-g.add_edge(0, 1, 2)
-g.add_edge(0, 2, 1)
-g.add_edge(0, 3, 5)
-g.add_edge(1, 2, 2)
-g.add_edge(1, 3, 3)
-g.add_edge(2, 3, 3)
-g.add_edge(2, 4, 1)
-g.add_edge(3, 4, 1)
-g.add_edge(3, 5, 5)
-g.add_edge(4, 5, 2)
-
-D = g.dijkstra(0)
-
-for no in range(len(D)):
-    print("Distância do nó 0 para o nó", no, "é", D[no])
+        for no in range(len(D)):
+            print("Distância do nó ", start_vertex, " para o nó ", grafo.V[no], "é", D[no])
